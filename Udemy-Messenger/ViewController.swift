e9fa2cb326d0464632dd6b325d28ca23ab162ec5
TheLN <lengocthe54@gmail.com>
2022-07-03 16:08:41 +0700

    Initial Commit

---
+++ Udemy-Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udemy-Messenger
+//
+//  Created by The Ngoc on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
